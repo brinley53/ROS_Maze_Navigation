69ba198e136c5b594a5c91d142780cba7faf144f
brinley53 <brinleyhull53@ku.edu>
2025-04-21 16:36:47 -0500

    current stuff

--- maze.py
+++ maze.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
-
-from graph import Graph
-
 import rclpy
 from rclpy.node import Node
 
@@ -16,8 +13,8 @@ import time
 from collections import deque   # BFS uses queue
 
 # Constants
-MOVING_SPEED = 0.2
-WALL_DISTANCE = 0.3  # Minimum distance from wall (meters) (4 in ~ 0.1 m). take car into account
+MOVING_SPEED = 0.3
+WALL_DISTANCE = 0.25  # Minimum distance from wall (meters) (4 in ~ 0.1 m)
 TURN_SPEED = 1.0  # Speed for turning away from walls
 FORWARD_DISTANCE = 0.3  # Distance to move forward (meters)
 
@@ -25,14 +22,12 @@ END_DISTANCE = 0.1 # Minimum distance from blue wall (meters) (4 in ~ 0.1 m)
 FORWARD_SPEED = 0.2
 COLOR_TOLERANCE = 20  # For color detection centering
 
-TURN_DURATION = 0.785 # maybe 45 degrees?
-STEP_DURATION = 0.1 # maybe 5 inches?
+TURN_DURATION = 0.785 # figure out what 90 egree is
+STEP_DURATION = 0.2
 
 #BLUE color range  
 BLUE_LOWER = np.array([73, 155, 94])
 BLUE_UPPER = np.array([70, 255, 100])
-
-DIRECTIONS = ["forward", 'right', 'backward', 'left']
 
 class MazeNavigator(Node):
     def __init__(self, name):
@@ -54,12 +49,10 @@ class MazeNavigator(Node):
         self.start = True
         
         # Maze navigation variables
-        self.graph = Graph()  # Graph representation of maze
+        #self.graph = Graph()  # Graph representation of maze
         self.current_node = (0, 0)  # Current position in grid
         self.previous_node = (0, 0)
         self.heading = 0  # 0=N, 1=E, 2=S, 3=W
-        # self.path = []  # BFS path to follow
-        # self.path_index = 0  # Current path position
 
         self.marked = set()
         self.dfs_stack = [self.current_node]
@@ -71,13 +64,14 @@ class MazeNavigator(Node):
         self.mapping_complete = False
 
         self.twist = Twist()
+        self.steps = 0
 
         self.moving = False
         self.move_start_time = None
         self.move_target = None
 
         self.create_timer(0.1, self.dfs_step)
-
+        
     def lidar_callback(self, data):
         """Process Lidar data for wall detection and navigation"""
         if self.at_end or not self.first_attempt:
@@ -94,32 +88,34 @@ class MazeNavigator(Node):
                 self.stop_motion()
                 self.current_node = self.move_target
                 self.moving = False
+                self.steps += 1
             return
 
         # If stack is empty, we?re done
-        if not self.dfs_stack:
-            self.get_logger().info("DFS complete!")
-            return
+        #if not self.dfs_stack:
+         #   self.get_logger().info("DFS complete!")
+          #  return
 
         current = self.dfs_stack[-1]
 
         # Mark the node on first visit
-        if current not in self.marked:
-            self.marked.add(current)
+        #if current not in self.marked:
+         #   self.marked.add(current)
 
         # Try to push an unvisited neighbor
         for nbr in self.find_neighbors(current):
-            if nbr not in self.marked and nbr not in self.dfs_stack:
-                self.dfs_stack.append(nbr)
-                self.move_to_node(current, nbr)
-                return
+            # if nbr not in self.marked and nbr not in self.dfs_stack:
+            #     self.dfs_stack.append(nbr)
+            self.move_to_node(current, nbr)
+            return
 
         # No unvisited neighbors ? backtrack
-        self.dfs_stack.pop()
-        if self.dfs_stack:
-            prev = self.dfs_stack[-1]
-            self.move_to_node(current, prev)
-
+        #self.dfs_stack.pop()
+        #if self.dfs_stack:
+         #   prev = self.dfs_stack[-1]
+          #  self.move_to_node(current, prev)
+                
+                
     def find_neighbors(self, current):
         open_directions = self.check_for_walls()
         neighbors = []
@@ -137,9 +133,11 @@ class MazeNavigator(Node):
             if direction == "W" and (self.heading==0 or self.heading==1 or self.heading==7) or direction == "NW" and (self.heading == 0 or self.heading==6 or self.heading==7) or direction == "N" and (self.heading==6 or self.heading==7 or self.heading==5) or direction=="NE" and (self.heading==4 or self.heading ==5 or self.heading==6) or direction=="E" and (self.heading==3 or self.heading==4 or self.heading==5) or direction=="SE" and (self.heading==3 or self.heading==4 or self.heading==2) or direction=="S" and (self.heading==3 or self.heading==1 or self.heading==2) or direction=="SW" and (self.heading==0 or self.heading==1 or self.heading==2):
                 tempX -= 1
             neighbors.append((tempX, tempY))
+            
+        print(neighbors)
 
         return neighbors
-
+        
     def move_to_node(self, previous, neighbor):
         dx = neighbor[0] - previous[0]
         dy = neighbor[1] - previous[1]
@@ -163,7 +161,7 @@ class MazeNavigator(Node):
             desired_heading = 7
         else:
             return
-
+            
         turns = (desired_heading - self.heading) % 8
         if turns > 4:
             turns -= 8
@@ -181,8 +179,9 @@ class MazeNavigator(Node):
     def turn(self, turns):
         if turns == 0:
             return
+        self.steps = 0
         twist = Twist()
-        twist.angular.z = TURN_SPEED if turns > 0 else -TURN_SPEED
+        twist.angular.z = -TURN_SPEED if turns > 0 else TURN_SPEED
         duration = abs(turns) * TURN_DURATION
         self.cmd_vel_pub.publish(twist)
         time.sleep(duration)
@@ -199,30 +198,41 @@ class MazeNavigator(Node):
 
         angle_increment = 2 * np.pi / len(self.lidar_data)
         direction_angles = {
-            "N": 0, #forward
-            "NE": -np.pi / 4,
-            "SE": -3 *np.pi / 4,
-            "SW": 3 *np.pi / 4,
-            "NW": np.pi / 4,
-            "S": np.pi, #backward
             "E": -np.pi / 2, #right
-            "W": np.pi / 2 #left
+            "N": 0, #forward
+            #"W": np.pi / 2, #left
+            #"NE": -np.pi / 4,
+            #"NW": np.pi / 4,
+            #"SE": -3 *np.pi / 4,
+            #"SW": 3 *np.pi / 4,
+            "S": np.pi #backward
+            
         }
 
         open_directions = []
-        range_deg = 15
-        range_rad = np.deg2rad(range_deg)
-        range_indices = int(range_rad / angle_increment)
 
         for direction, angle in direction_angles.items():
+            if direction == "E":
+                range_deg = 20
+                range_rad = np.deg2rad(range_deg)
+                range_indices = int(range_rad / angle_increment)
+            else:
+                range_deg = 15
+                range_rad = np.deg2rad(range_deg)
+                range_indices = int(range_rad / angle_increment)
             center_index = int((angle % (2 * np.pi)) / angle_increment)
             indices = [(center_index + i) % len(self.lidar_data) for i in range(-range_indices, range_indices + 1)]
             distances = [self.lidar_data[i] for i in indices if not np.isnan(self.lidar_data[i])]
-            if distances and np.mean(distances) > WALL_DISTANCE:
+            if distances and min(distances) > WALL_DISTANCE:
                 open_directions.append(direction)
+                
+        if len(open_directions) > 1 and open_directions[0] == "E":
+            if self.steps < 3:
+                open_directions.pop()
+        print(open_directions)
 
         return open_directions
-
+        
     def camera_callback(self, data):
         """Detect BLUE room"""
         if self.at_end:
@@ -256,3 +266,4 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
+
